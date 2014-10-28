4f2b33818852ce9ef804335f8869dbdba51d46e4
davide9 <bianchi_davide@hotmail.it>
2014-10-28 13:14:26 +0100

    Correzione Redirect di ricerca.php

--- AgenCavi_sito/ricerca.php
+++ AgenCavi_sito/ricerca.php
@@ -20,27 +20,20 @@
  * @param array $headers Optional. Extra headers to send.
  */
 function redirect_post($url, array $data, array $headers = null) {
-    $params = array(
-        'http' => array(
-            'method' => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    if (!is_null($headers)) {
-        $params['http']['header'] = '';
-        foreach ($headers as $k => $v) {
-            $params['http']['header'] .= "$k: $v\n";
-        }
-    }
-    $ctx = stream_context_create($params);
-    $fp = @fopen($url, 'rb', false, $ctx);
-    if ($fp) {
-        echo @stream_get_contents($fp);
-        die();
-    } else {
-        // Error
-        throw new Exception("Error loading '$url', $php_errormsg");
-    }
+	?>
+	<form method="POST" action="<?php echo $url; ?>" name="re_frm">
+	<?php 
+		foreach ( $data as $ke => $va ){
+	?>
+		<input type="hidden" name="<?php echo $ke; ?>" value="<?php echo $va; ?>" />
+	<?php
+		}
+	?>
+	</form>
+	<script language="JavaScript">
+		document.re_frm.submit();
+	</script>
+	<?php
 }
 	
 	$var = "display: none;";
